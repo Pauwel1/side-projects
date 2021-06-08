2c5febe387f49e0fcfa088b8622aa1a9eb04e7fa
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-06-08 12:08:40 +0200

    Next steps

--- Appertements.py
+++ Appertements.py
@@ -1,17 +1,26 @@
 import requests
 import re
+import bs4
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import sys
 import time
 import numpy as np
 import pandas as pd
 
 driver = webdriver.Chrome()
-url = "https://www.immoweb.be/nl/zoeken/huis-en-appartement/te-huur?countries=BE&minPrice=900&page=1&orderBy=relevance"
+headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"}
+url = "https://www.immoweb.be/nl/zoeken/huis-en-appartement/te-huur?countries=BE&hasTerraceOrGarden=true&minBedroomCount=2&minPrice=900&postalCodes=1000,1040,1050,1150,1160,1170,1180,1200,1560,1600,1601,1620,1630,1640,1650,1651,1652&orderBy=cheapest"
 driver.get(url)
 
-soup = BeautifulSoup(driver.page_source, "html.parser")
-listings = soup.find_all('a', class_="card_title-link")
-listings[:5]
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+
+listings = {}
+
+property_type = soup.select(".classified__header-primary-info .classified__title").text.strip()
+listings["property_types"] = property_type
+    
+print(listings)
+
+driver.close()
